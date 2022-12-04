32c206fc5f7cad84fe068bbb8d04660c5bef5bbd
rezwanahmodsami <samiahmed0f0@gmail.com>
2022-12-04 22:51:15 +0600

    updated meta

--- includes/Global.php
+++ includes/Global.php
@@ -13,6 +13,11 @@ $downloadLink = "https://github.com/rezwanahmodsami/routemamba/releases/tag/v3.1
 
 $releasesData = [
     [
+        "version" => "v3.1.2",
+        "date" => "04/12/2022",
+        "details" => "<ul><li>Fixed invalid server host problem on domain</li></ul>"
+    ],
+    [
         "version" => "v3.1.1",
         "date" => "23/11/2022",
         "details" => "<ul><li>Fixed some minor bugs.</li></ul>"
@@ -21,13 +26,6 @@ $releasesData = [
         "version" => "v3.1.0",
         "date" => "23/11/2022",
         "details" => "<ul><li>New Reactive state management system added.</li></ul>"
-    ],
-    [
-        "version" => "v3.0.0",
-        "date" => "17/11/2022",
-        "details" => "<ul>
-        <li>Routemamba re-writed by typescript to fix some bugs and keep safe from human error.</li>
-        </ul>"
     ],
 ]
 ?>
